756ad20703e0665719a19221e90aa4e1517a1438
Mfordantel <73074124+Mfordantel@users.noreply.github.com>
2021-01-25 13:46:45 +0600

    changes

--- main.go
+++ main.go
@@ -1,58 +1,98 @@
 package main
 
 import (
-        "fmt"
-        "net/http"
-        "io"
-        "os"
-        "strings"
-        "time"
-        "bytes"
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
-func info(endOfFile bool, buf *bytes.Buffer) {
-    for !endOfFile {
-        fmt.Println(buf.Len() / 1024, "Kb")
-        time.Sleep(time.Second)
-    }
+type counter struct {
+	Seconds int
+	Total uint64
 }
 
-func main() {
-        var url string
-        fmt.Print("Enter file URL: ")
-        fmt.Scanf("%s\n",&url)
+func (writecount *counter) Write(p []byte) (int, error) {
+	n := len(p)
+	writecount.Total += uint64(n)
+	return n, nil
+}
 
-        fileName := url[1+strings.LastIndex(url, "/"):]
-        if fileName == "" {
+func sec(writecount *counter){
+	for {
+		time.Sleep(time.Second)
+		writecount.Seconds++
+		fmt.Println("Time:",writecount.Seconds,"sec")
+	}
+}
+
+func download(way string, url string) error {
+
+	if !strings.Contains(way, ".") {
+		way += ".html"
+	}
+
+
+	out, err := os.Create(way)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+
+	var buf bytes.Buffer
+	endFile := false
+	read := io.TeeReader(resp.Body, &buf)
+	fmt.Println("Begin download. Already installed:")
+	
+	count := &counter{}
+	go sec(count)
+
+	go func() {
+		for !endFile {
+			fmt.Println("Downloaded:", buf.Len()/1024, "Kb")
+			time.Sleep(time.Second)
+		}
+	}()
+
+	_, err = io.Copy(out, read)
+	endFile = true
+
+	fmt.Println(buf.Len()/1024, "Kb")
+	fmt.Println("Downloaded as " + way)
+	return err
+
+}
+func main() {
+
+	var url string
+	fmt.Print("Enter file URL: ")
+	fmt.Scanf("%s\n", &url)
+
+	SepUrl := strings.Split(url, "/")
+
+	var file_name string
+	for i := 1; file_name == "" && i < len(SepUrl); i++ {
+		file_name = SepUrl[len(SepUrl)-i]
+	}
+        if file_name == "" {
                 fmt.Println("Error: Wrong URL format")
                 os.Exit(1)
         }
 
-        status, err := http.Get(url)
-        if err != nil {
-                fmt.Println("Error: Can't get file")
-                os.Exit(1)
-        }
-        defer status.Body.Close()
-
-        file, err := os.Create(fileName)
-        if err != nil {
-                fmt.Println("Error: Can't create file")
-                os.Exit(1)
-        }
-        defer file.Close()
-
-        var buf bytes.Buffer
-        endOfFile := false
-        reader := io.TeeReader(status.Body, &buf)
-        fmt.Println("Start downloading. Already received:")
-        go info(endOfFile, &buf)
-        _, err = io.Copy(file, reader)
-        if err != nil {
+	err := download(file_name, url)
+	if err != nil {
                 fmt.Println("Error: Can't copy to file")
                 os.Exit(1)
-        }
-        endOfFile = true
-
-        fmt.Println("Download!")
+	}
 }
